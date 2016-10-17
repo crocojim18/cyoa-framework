f987c383842420046326d2757321ac268bc610e2
Marty Taylor <crocojim18@gmail.com>
2016-10-17 14:38:07 -0400

    Changed functions in order to hide empty space

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Choose-Your-Own-Adventure Game Framework
+
+---
+
+## What is this?
+
+This is a framework to make choose-your-own adventure games, much like [The Worst Meeting EVER](https://github.com/crocojim18/the-worst-meeting-ever). I've been making these games for a couple of years, and I've tried to make the process as universalized as possible. Every game brings new functionality to the engine, and that's only going to progress as more games are made with it.
+
+## What does the current version do?
+
+The framework currently can control the following elements:
+	* Buttons
+	* Textbox Input
+	* Any content (pictures, text, etc.)
+	* Background color
+As time goes on, more features are sure to be added. Suggestions are welcome!
+
+## Licensing
+
+Creative Commons (CC BY). This is obviously meant to be a framework for any number of games, and it'd be cool, if you made something with it, to tell me. I'd love to share your stuff!
--- functions.js
+++ functions.js
@@ -24,44 +24,44 @@ function changeValues4(message)
 
 function hide1()
 {
-	document.getElementById("choice1").style.visibility="hidden";
+	document.getElementById("choice1").style.display="none";
 }
 
 function hide2()
 {
-	document.getElementById("choice2").style.visibility="hidden";
+	document.getElementById("choice2").style.display="none";
 }
 
 function hide3()
 {
-	document.getElementById("choice3").style.visibility="hidden";
+	document.getElementById("choice3").style.display="none";
 }
 
 function hide4()
 {
-	document.getElementById("choice4").style.visibility="hidden";
+	document.getElementById("choice4").style.display="none";
 }
 
 //These functions show the buttons
 
 function show1()
 {
-	document.getElementById("choice1").style.visibility="visible";
+	document.getElementById("choice1").style.display="block";
 }
 
 function show2()
 {
-	document.getElementById("choice2").style.visibility="visible";
+	document.getElementById("choice2").style.display="block";
 }
 
 function show3()
 {
-	document.getElementById("choice3").style.visibility="visible";
+	document.getElementById("choice3").style.display="block";
 }
 			
 function show4()
 {
-	document.getElementById("choice4").style.visibility="visible";
+	document.getElementById("choice4").style.display="block";
 }
 
 //These functions change color, such as background and text color
@@ -80,12 +80,12 @@ function changeTextColor(color)
 
 function hideTextInput()
 {
-	document.getElementById("textInput").style.visibility="hidden";
+	document.getElementById("textInputSection").style.display="none";
 }
 
 function showTextInput()
 {
-	document.getElementById("textInput").style.visibility="visible";
+	document.getElementById("textInputSection").style.display="block";
 }
 
 function getTextInput()
@@ -97,5 +97,10 @@ function getTextInput()
 
 function hideContent()
 {
-	document.getElementById("content").style.visibility="hidden";
+	document.getElementById("content").style.display="none";
+}
+
+function showContent()
+{
+	document.getElementById("content").style.display="block";
 }
